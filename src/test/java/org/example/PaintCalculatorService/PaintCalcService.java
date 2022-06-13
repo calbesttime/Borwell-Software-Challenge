c441555e3f035e9c6acb0b6fd61f4e877b0c728f
Yukun <geyukun7@gamil.com>
2022-06-13 17:10:41 +0100

    improvement: replaced some doubles in PaintCalcService.java with final doubles to refine the code.

--- src/test/java/org/example/PaintCalculatorService/PaintCalcService.java
+++ src/test/java/org/example/PaintCalculatorService/PaintCalcService.java
@@ -4,8 +4,9 @@ public class PaintCalcService {
     final private double pi = 3.14;
     final private double cuboid = 1.0;
     final private double cylinder = 2.0;
-
     final private double cuboidWith1MC = 3.0;
+    final private double cuboidWith2MC = 4.0;
+
     public double calcArea(CalcAreaDto calcAreaDto) {
         if (calcAreaDto.getShape() == cuboid && calcAreaDto.getLength() > 0 && calcAreaDto.getWidth() > 0) {
             return calcAreaDto.getLength() * calcAreaDto.getWidth();
@@ -14,7 +15,7 @@ public class PaintCalcService {
         } else if (calcAreaDto.getShape() == cuboidWith1MC && calcAreaDto.getLength() > 0 && calcAreaDto.getWidth() > 0
                 && calcAreaDto.getCorner1Len() > 0 && calcAreaDto.getCorner1Len() > 0) {
             return calcAreaDto.getLength() * calcAreaDto.getWidth() - calcAreaDto.getCorner1Len() * calcAreaDto.getCorner1Wid();
-        } else if (calcAreaDto.getShape() == 4.0 && calcAreaDto.getLength() > 0 && calcAreaDto.getWidth() > 0
+        } else if (calcAreaDto.getShape() == cuboidWith2MC && calcAreaDto.getLength() > 0 && calcAreaDto.getWidth() > 0
                 && calcAreaDto.getCorner1Len() > 0 && calcAreaDto.getCorner1Len() > 0
                 && calcAreaDto.getCorner2Len() > 0 && calcAreaDto.getCorner2Wid() > 0) {
             return calcAreaDto.getLength() * calcAreaDto.getWidth() -

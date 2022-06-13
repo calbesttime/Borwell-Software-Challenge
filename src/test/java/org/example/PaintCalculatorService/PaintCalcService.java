2d188b39c84c34d8b058df1035fc9773a9b45618
Yukun <geyukun7@gamil.com>
2022-06-13 16:54:04 +0100

    improvement: replaced some doubles in PaintCalcService.java with final doubles to refine the code.

--- src/test/java/org/example/PaintCalculatorService/PaintCalcService.java
+++ src/test/java/org/example/PaintCalculatorService/PaintCalcService.java
@@ -4,12 +4,14 @@ public class PaintCalcService {
     final private double pi = 3.14;
     final private double cuboid = 1.0;
     final private double cylinder = 2.0;
+
+    final private double cuboidWith1MC = 3.0;
     public double calcArea(CalcAreaDto calcAreaDto) {
         if (calcAreaDto.getShape() == cuboid && calcAreaDto.getLength() > 0 && calcAreaDto.getWidth() > 0) {
             return calcAreaDto.getLength() * calcAreaDto.getWidth();
         } else if (calcAreaDto.getShape() == cylinder && calcAreaDto.getRadius() > 0) {
             return pi * calcAreaDto.getRadius() * calcAreaDto.getRadius();
-        } else if (calcAreaDto.getShape() == 3.0 && calcAreaDto.getLength() > 0 && calcAreaDto.getWidth() > 0
+        } else if (calcAreaDto.getShape() == cuboidWith1MC && calcAreaDto.getLength() > 0 && calcAreaDto.getWidth() > 0
                 && calcAreaDto.getCorner1Len() > 0 && calcAreaDto.getCorner1Len() > 0) {
             return calcAreaDto.getLength() * calcAreaDto.getWidth() - calcAreaDto.getCorner1Len() * calcAreaDto.getCorner1Wid();
         }

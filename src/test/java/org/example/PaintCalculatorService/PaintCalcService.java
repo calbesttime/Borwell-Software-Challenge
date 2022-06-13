851f2bbff79743ca5d9c22323c4934978d301ebe
Yukun <geyukun7@gamil.com>
2022-06-13 16:27:52 +0100

    updated PaintCalcService.java
    completed the method to calculate the floor area of the cylinder room

--- src/test/java/org/example/PaintCalculatorService/PaintCalcService.java
+++ src/test/java/org/example/PaintCalculatorService/PaintCalcService.java
@@ -4,6 +4,8 @@ public class PaintCalcService {
     public double calcArea(CalcAreaDto calcAreaDto) {
         if (calcAreaDto.getShape() == 1.0 && calcAreaDto.getLength() > 0 && calcAreaDto.getWidth() > 0) {
             return calcAreaDto.getLength() * calcAreaDto.getWidth();
+        } else if (calcAreaDto.getShape() == 2.0 && calcAreaDto.getRadius() > 0) {
+            return 3.14 * calcAreaDto.getRadius() * calcAreaDto.getRadius();
         }
         return 0;
     }

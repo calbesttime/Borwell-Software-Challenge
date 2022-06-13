ddc59d57470be95a751050049ac682d3daeb3349
Yukun <geyukun7@gamil.com>
2022-06-13 16:33:09 +0100

    improvement: replaced some doubles in PaintCalcService.java with final variables to refine the code

--- src/test/java/org/example/PaintCalculatorService/PaintCalcService.java
+++ src/test/java/org/example/PaintCalculatorService/PaintCalcService.java
@@ -1,11 +1,14 @@
 package org.example.PaintCalculatorService;
 
 public class PaintCalcService {
+    final private double pi = 3.14;
+    final private double cuboid = 1.0;
+    final private double cylinder = 2.0;
     public double calcArea(CalcAreaDto calcAreaDto) {
-        if (calcAreaDto.getShape() == 1.0 && calcAreaDto.getLength() > 0 && calcAreaDto.getWidth() > 0) {
+        if (calcAreaDto.getShape() == cuboid && calcAreaDto.getLength() > 0 && calcAreaDto.getWidth() > 0) {
             return calcAreaDto.getLength() * calcAreaDto.getWidth();
-        } else if (calcAreaDto.getShape() == 2.0 && calcAreaDto.getRadius() > 0) {
-            return 3.14 * calcAreaDto.getRadius() * calcAreaDto.getRadius();
+        } else if (calcAreaDto.getShape() == cylinder && calcAreaDto.getRadius() > 0) {
+            return pi * calcAreaDto.getRadius() * calcAreaDto.getRadius();
         }
         return 0;
     }

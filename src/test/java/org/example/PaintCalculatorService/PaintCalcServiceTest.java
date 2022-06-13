7ddd6eceff551bf3ea69bb1f2e136f3c85f36b54
Yukun <geyukun7@gamil.com>
2022-06-13 17:03:29 +0100

    feat: initialed a test for calculating the floor area of a cuboid room with 2 missing corner
    updated the method buildCalcAreaDto()

--- src/test/java/org/example/PaintCalculatorService/PaintCalcServiceTest.java
+++ src/test/java/org/example/PaintCalculatorService/PaintCalcServiceTest.java
@@ -64,6 +64,32 @@ public class PaintCalcServiceTest {
         Assert.assertEquals(13, area, 0);
     }
 
+    @Test
+    public void area_should_be_12m2_if_the_room_is_a_3m_by_5m_cuboid_with_2mx1m_and_1mx1m_missing_corners() {
+        final PaintCalcService paintCalcService = new PaintCalcService();
+        final String shape = "cuboid_with_2_missing_corner";
+        final double length = 5;
+        final double width = 3;
+        final double corner1Len = 2;
+        final double corner1Wid = 1;
+        final double corner2Len = 1;
+        final double corner2Wid = 1;
+        HashMap<String, Double> dimensions = new HashMap<>();
+        // "shape" == 4.0 while the room is a cuboid with 2 missing corner
+        dimensions.put("shape", 4.0);
+        dimensions.put("length", 5.0);
+        dimensions.put("width", 3.0);
+        dimensions.put("corner1Len", 2.0);
+        dimensions.put("corner1Wid", 1.0);
+        dimensions.put("corner2Len", 1.0);
+        dimensions.put("corner2Wid", 1.0);
+        final CalcAreaDto calcAreaDto = buildCalcAreaDto(dimensions);
+
+        double area = paintCalcService.calcArea(calcAreaDto);
+
+        Assert.assertEquals(12, area, 0);
+    }
+
     private CalcAreaDto buildCalcAreaDto(HashMap dimensions) {
         CalcAreaDto calcAreaDto = new CalcAreaDto();
         if (dimensions.get("shape").equals(1.0)) {
@@ -79,7 +105,14 @@ public class PaintCalcServiceTest {
             calcAreaDto.setWid((Double) dimensions.get("width"));
             calcAreaDto.setCorner1Len((Double) dimensions.get("corner1Len"));
             calcAreaDto.setCorner1Wid((Double) dimensions.get("corner1Wid"));
-        }
+        } else if (dimensions.get("shape").equals(4.0)) {
+            calcAreaDto.setShape((Double) dimensions.get("shape"));
+            calcAreaDto.setLen((Double) dimensions.get("length"));
+            calcAreaDto.setWid((Double) dimensions.get("width"));
+            calcAreaDto.setCorner1Len((Double) dimensions.get("corner1Len"));
+            calcAreaDto.setCorner1Wid((Double) dimensions.get("corner1Wid"));
+            calcAreaDto.setCorner2Len((Double) dimensions.get("corner2Len"));
+            calcAreaDto.setCorner2Wid((Double) dimensions.get("corner2Wid"));
         return calcAreaDto;
     }
 }

778401e348283b1ed611d4ab32bcdf7a6f4c8669
Yukun <geyukun7@gamil.com>
2022-06-13 16:05:11 +0100

    fix: complete PaintCalcService.java
    test for calculating the area of the cuboid room passed

--- src/test/java/org/example/PaintCalculatorService/PaintCalcService.java
+++ src/test/java/org/example/PaintCalculatorService/PaintCalcService.java
@@ -2,6 +2,9 @@ package org.example.PaintCalculatorService;
 
 public class PaintCalcService {
     public double calcArea(CalcAreaDto calcAreaDto) {
+        if (calcAreaDto.getShape() == 1.0 && calcAreaDto.getLength() > 0 && calcAreaDto.getWidth() > 0) {
+            return calcAreaDto.getLength() * calcAreaDto.getWidth();
+        }
         return 0;
     }
 }
